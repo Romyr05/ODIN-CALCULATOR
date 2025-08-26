76e62269f34467d0f3d11cbe44bb5070d3538b58
Romyr <romyrlopez123@gmail.com>
2025-08-27 02:53:20 +0800

    progress[ aug 27,2025]

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
 //Elements
-const prompt = require('prompt-sync')({sigint: true});
+let num1,ope,num2;
+
+const equation = document.getElementById("equation")
+const result = document.getElementById("result")
+const calc_num = document.querySelectorAll("#calc_num")
+const calc_ope = document.querySelectorAll("#calc_ope")
+
+
+
+calc_num.forEach( (button) => {
+    button.addEventListener(("click"), () => {append(button.textContent)})
+});
+
+calc_ope.forEach((button) => {
+    button.addEventListener(("click"),() => {(button.textContent)})
+})
+
+
 
 //Functions
+function append(number){
+    if(first_number.textContent !== 0){
+        first_number.textContent += number
+        console.log()
+    }
+}
+
+
 function add(num1, num2){
     return num1 + num2
 }
@@ -17,14 +42,6 @@ function multiply(num1, num2){
 function divide(num1, num2){
     return num1/num2
 }
-
-let testing = prompt("enter a number")
-
-let num1,ope,num2;
-
-num1 = parseFloat(testing[0])
-ope = testing[1].trim()
-num2 = parseFloat(testing[2])
 
 
 function operate(num1, num2, ope){
@@ -46,7 +63,5 @@ function operate(num1, num2, ope){
     return answer
 }
 
-test = operate(num1,num2,ope)
-console.log(test);
 
 
